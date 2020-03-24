77aa092f3afb407b0b374c87bff4f39a3d624e44
vikram-pixel <60808438+vikram-pixel@users.noreply.github.com>
2020-03-24 14:43:14 +0530

    Add files via upload

--- assignments/vikramAssingment/script.js
+++ assignments/vikramAssingment/script.js
@@ -1,4 +1,3 @@
-
     function formValidation()
         {
             var fname=document.registration.firstname;
@@ -7,111 +6,58 @@
             var uemail=document.registration.email;
             var mnumber=document.registration.mobilenumber;
             var postalcode=document.registraton.code;
+    
+        var name = /^[a-zA-Z]*$/;                
+        if(fname.value.match(name) {
+  
+        } else {
+             printError( "Please enter a valid name,donot use Numbers");
+        }
+    
+        var lnamee = /[a-zA-Z]+$/;                
+        if(lname.value.match(lnamee){
+          
+        } else {
+           printError( "Please enter a valid User name");
+        }
+    
+        var username=/(?=.\d)(?=.[a-z])(?=.[A-Z])(?=.[!@#$%^&*(),.?":{}|<>]).{8,16}/;
+          if(uname.value.match(username) {
+         
+        } else {
+          printError( "Please enter a valid User name");
+        }
+    
+        var usermail = /^\S+@\S+\.\S+$/;
+          if(umail.value.match(usermail) {
+           
+        } else{
+            printError("emailErr", "Please enter a valid email address");
+        }
+
+        var number = /^[1-9]\d{9}$/;
+          if(mnumber.value.match(number) {
             
-            if(allLetter1(fname))
-                {
-                    if(allLetter2(lname))
-                        {
-                            if(allLetter3(uname))
-                                {
-                                    if(ValidateEmail(uemail))
-                                {
-                                    if(phonenumber(mnumber))
-                                    {
-                                         if(zipvalid(postalcode))
-                                         {
-                                            
-                                        }
-                                    }
-                                       
-                                }
-                        }
-                }
+        } else{
+          printError("mobileErr", "Please enter a valid 10 digit mobile number,donot use Alphabets");
         }
-            return false;
+		
+        var code = /[0-9]{8}$/;
+       if(postalcode.value.match(code) {
+         
+        } else{
+              printError("pcodeErr", "Please enter a valid 8 digit postalcode");
         }
-        function allLetter1(fname)
-        {
-            var letters = /^[A-Za-z]+$/;
-            if(fname.value.match(letters))
-                {
-                    return true;
-                }
-            else{
-                alert('FirstName must have a only alphabets & maximum 6 characters only');
-                fname.focus();
-                return false;
-            }
-        }
-        function allletter2(lname)
-        {
-            var letters=/^[A-Za-z]+$/;
-            if(lname.value.match(letters))
-                {
-                    return true;
-                }
-            else{
-                alert('LastName must have a only alphabets & maximum 6 characters only');
-                lname.focus();
-                return false;
-            }
-        }
-        function allletter3(uname)
-        {
-            var letters=/^[A-Za-z]+$/;
-            var letters = /^[A-Za-z]+$/;
-           if(uname.value.match(letters))
-           {
-             return true;
-           }
-    else
-       {
-          alert('Username must have alphabet characters only');
-          uname.focus();
-           return false;
-      } 
- }       
-        function zipvalid(postalcode)
-{ 
-var numbers = /^[0-9]+$/;
-if(uzip.value.match(numbers))
-{
-return true;
-}
-else
-{
-alert('ZIP code must have numeric characters only');
-postalcode.focus();
-return false;
-}
-}
-       
-        function phonenumber(mnumber)
-{
-var phoneno = /^\d{10}$/;
-if(inputtxt.value.match(phoneno))
-{
-return true;
-}
-else
-{
-alert("Enter a valid mobile number");
-mnumber.focus();
-return false;
-}
-}   
-     function ValidateEmail(uemail)
-{
-var mailformat = /^\w+([\.-]?\w+)@\w+([\.-]?\w+)(\.\w{2,3})+$/;
-if(uemail.value.match(mailformat))
-{
-return true;
-}
-else
-{
-alert("You have entered an invalid email address!");
-uemail.focus();
-return false;
-}
-}
-}
+		
+                var dataPreview = "You've entered the following details: \n" +
+                          "First Name: " + fname + "\n" +
+                          "Last Name: " + lname + "\n" +
+                          "User Name:" +uname+"\n"+
+                          "Email Address: " + uemail + "\n" +
+                          "Mobile Number: " + mobile + "\n" +
+                          "Postal Code: " + postalcode + "\n" 
+
+          }
+        alert(dataPreview);
+    
+};
