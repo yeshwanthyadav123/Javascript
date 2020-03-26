c512d9a5cf6eb91b0761e095be334a8bc2cf3748
vemulashashidar <39757304+vemulashashidar@users.noreply.github.com>
2020-03-26 22:57:07 +0530

    Add files via upload

---
+++ assignments/shashi/Using_AjaxCall/validate.js
@@ -0,0 +1,69 @@
+function fetchData(){
+    var xmlhttp=new XMLHttpRequest();
+    
+    xmlhttp.onreadystatechange = function() {
+        if (xmlhttp.readyState == XMLHttpRequest.DONE) {
+            if (xmlhttp.status == 200) 
+            {
+                var ObjectData=xmlhttp.response;
+                var parsedData=JSON.parse(ObjectData);
+                var columns = [];
+                for (var rows = 0; rows < parsedData.length; rows++) {
+                    var records = {};
+                    records.Id = parsedData[rows].id;
+                    records.NAME = parsedData[rows].name;
+                    records.USERNAME = parsedData[rows].username;
+                    records.CITY = parsedData[rows].address.city;
+                    records.POSTALCODE = parsedData[rows].address.zipcode;
+                    records.MOBILENUMBER = parsedData[rows].phone;
+                    records.BS = parsedData[rows].company.bs;
+                    columns.push(records);
+                }
+                var table=document.createElement("table");
+                table.setAttribute("id","myTable");
+                var tablerow=table.insertRow(-1);
+                let headerKeys = Object.keys(columns[0])
+                var tablehead = document.createElement("th");
+                tablehead.innerHTML="DeleteRow";
+                tablerow.appendChild(tablehead);
+                headerKeys.forEach( element => {
+                    var tablehead = document.createElement("th");
+                    tablehead.innerHTML = element;
+                    tablerow.appendChild(tablehead);
+                })
+                
+                // iterating the records of object
+                for (var i = 0; i < columns.length; i++) { 
+                    tablerow = table.insertRow(-1);
+                var button = document.createElement('input');
+                button.setAttribute('type', 'button');
+                button.setAttribute('value', 'Delete');
+                button.setAttribute('id','deleteRow');
+                button.setAttribute('onclick', 'removeRow(this);');
+                tablerow.appendChild(button);
+
+                    
+                     //Iterating cell data
+                    
+                    for (let eachKey in columns[i]) {      
+                        
+                        var tablecell = tablerow.insertCell(-1);
+                        tablecell.innerHTML = columns[i][eachKey];
+                    }
+                }
+                var divContainer = document.getElementById("paragraph");
+                divContainer.innerHTML = "";
+                divContainer.appendChild(table); 
+            }
+            else if (xmlhttp.status == 400) {
+               alert('There was an error 400');
+            }
+        }
+    }
+    xmlhttp.open("GET","https://jsonplaceholder.typicode.com/users",true);
+    xmlhttp.send();
+}
+function removeRow(oButton) 
+{
+    document.getElementById('myTable').deleteRow(oButton.parentNode.rowIndex);    
+}
